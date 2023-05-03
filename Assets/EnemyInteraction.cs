1c807037220be78c45fb15893ee24b83325ccfd3
Vincent-Liu87 <43618496+TLitchmore@users.noreply.github.com>
2023-05-03 17:40:32 +0200

    changed enemy, added boombox and animations

--- Assets/EnemyInteraction.cs
+++ Assets/EnemyInteraction.cs
@@ -1,64 +1,61 @@
 using UnityEngine;
 using System.Collections;
 
-public class EnemyInteraction : MonoBehaviour
-{
-    private Renderer renderer;
+public class EnemyInteraction : MonoBehaviour {
+    //private Renderer renderer;
     private Color originalColor;
-    private bool isColliding = false;
+    //private bool isColliding = false;
     private int hitCount = 0;
     private float hitCooldown = 0.1f;
     private float hitTimer = 0.0f;
     public ParticleSystem particleSystem;
+    Animator animator;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         // Get the Renderer component and original color from the enemy object
-        renderer = GetComponent<Renderer>();
-        originalColor = renderer.material.color;
+        //renderer = GetComponent<Renderer>();
+        //originalColor = renderer.material.color;
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // If a hit has been registered and the cooldown period has passed, reset the color and hit registration
-        if (isColliding && Time.time >= hitTimer + hitCooldown)
-        {
-            renderer.material.color = originalColor;
-            isColliding = false;
-        }
+        //if (isColliding && Time.time >= hitTimer + hitCooldown) {
+        //    //renderer.material.color = originalColor;
+        //    isColliding = false;
+        //}
     }
 
     // This method is called when a collision occurs with the enemy's collider
-    private void OnCollisionEnter(Collision collision)
-    {
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("HIT");
         // Check if the colliding object has a Rigidbody component and if the cooldown period has passed
         Rigidbody otherRb = collision.gameObject.GetComponent<Rigidbody>();
-        if (otherRb != null && Time.time >= hitTimer + hitCooldown)
-        {
+        if (otherRb != null && Time.time >= hitTimer + hitCooldown) {
             // If the colliding object has a Rigidbody, increment the hit count and start the cooldown timer
             hitCount++;
             hitTimer = Time.time;
-            StartCoroutine(ActivateParticles());
+            StartCoroutine(ManageHitEffects());
 
             // If the hit count is greater than or equal to 10, destroy the enemy
-            if (hitCount >= 10)
-            {
+            if (hitCount >= 10) {
                 Destroy(gameObject);
-            }
-            else
-            {
+            } else {
                 // Otherwise, change the color of the enemy to red and set isColliding to true
-                renderer.material.color = Color.red;
-                isColliding = true;
+                //renderer.material.color = Color.red;
+                //isColliding = true;
+
             }
         }
     }
 
-    private IEnumerator ActivateParticles() {
+    private IEnumerator ManageHitEffects() {
         particleSystem.Play();
+        animator.SetBool("isHit", true);
         yield return new WaitForSeconds(1f);
+        animator.SetBool("isHit", false);
         particleSystem.Stop();
         particleSystem.Clear();
     }
