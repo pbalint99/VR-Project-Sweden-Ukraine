837d0194c304348652d5371033e45b828516a9f6
yasyazdaniyan <yazdaniyanyas@gmail.com>
2023-05-19 14:55:31 +0200

    flowercrown progress

--- Assets/FlowerCrownOnHead.cs
+++ Assets/FlowerCrownOnHead.cs
@@ -3,17 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
-public class FlowerCrownOnHead : MonoBehaviour
-{
+public class FlowerCrownOnHead : MonoBehaviour {
+    private Interactable interactable;
+    private Rigidbody rigidbody;
+
+    //public GameObject flowerCrownPrefab;
+    public GameObject VRCamera;
+    GameObject duplicatedObject;
+
+    bool attachedToHand = false;
+    bool hasBeenGrabbed = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Wreath of flowers")
+        if (other.gameObject.name.Contains("Wreath of flowers") && attachedToHand == false && hasBeenGrabbed == true)
         {
             GetComponent<MeshRenderer>().enabled = true;
             Destroy(other.gameObject);
+
             this.gameObject.AddComponent<Throwable>();
-            this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+
+            rigidbody = GetComponent<Rigidbody>();
+            interactable = GetComponent<Interactable>();
+
+            // Subscribe to the OnDetachedFromHand event
+            interactable.onDetachedFromHand += OnDetachedFromHand;
+
+
+            // Disable gravity when the player grabs the object
+            rigidbody.useGravity = false;
+
+            duplicatedObject = Instantiate(this.gameObject, transform.position, transform.rotation, transform.parent);
+            duplicatedObject.SetActive(false);
         }
-        
+    }
+
+    private void OnDetachedFromHand(Hand hand)
+    {
+        attachedToHand = false;
+        // Enable gravity when the player lets go of the object
+        rigidbody.useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+        duplicatedObject.SetActive(true);
+        duplicatedObject.GetComponent<MeshRenderer>().enabled = false;
+        Destroy(duplicatedObject.GetComponent<Throwable>());
+        Destroy(duplicatedObject.GetComponent<Rigidbody>());
+        Destroy(duplicatedObject.GetComponent<Interactable>());
+        duplicatedObject.GetComponent<BoxCollider>().enabled = false;
+
+        //Instantiate new one
+        //GameObject flowerCrown = Instantiate(flowerCrownPrefab, VRCamera.transform, false);
+    }
+
+    protected virtual void OnAttachedToHand(Hand hand)
+    {
+        attachedToHand = true;
+        hasBeenGrabbed = true;
     }
 }
